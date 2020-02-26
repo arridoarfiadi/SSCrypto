7e735562f6fb44387a9e8cc730e384046ec26d83
Arrido Arfiadi <arridoarfiadi@hotmail.com>
2020-02-26 12:42:59 -0800

    Encryption Functionality

--- SSCrypto/ViewController.swift
+++ SSCrypto/ViewController.swift
@@ -13,16 +13,14 @@ class ViewController: NSViewController {
 	@IBOutlet weak var cipherTextTextView: NSTextView!
 	@IBOutlet weak var decryptedTextView: NSTextView!
 	
+	@IBOutlet weak var pubicKeyTextView: NSTextView!
+	@IBOutlet weak var plainTextTextView: NSTextView!
+	@IBOutlet weak var encryptedTextView: NSTextView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		decryptedTextView.isEditable = false
-		// Do any additional setup after loading the view.
-	}
-
-	override var representedObject: Any? {
-		didSet {
-		// Update the view, if already loaded.
-		}
+		encryptedTextView.isEditable = false
 	}
 	
 	@IBAction func decryptButtonClicked(_ sender: Any) {
@@ -33,16 +31,25 @@ class ViewController: NSViewController {
 		}
 	}
 	
+	@IBAction func encryptButtonClicked(_ sender: Any) {
+		if let encryptedText = plainTextTextView.string.encrypt(using: pubicKeyTextView.string) {
+			encryptedTextView.string = encryptedText
+		} else {
+			encryptedTextView.string = "Unable to encrypt"
+		}
+	}
+	
+}
+
+class SMCryptography {
+	internal let KEY_SIZE = 2048
+	internal let KEY_TYPE = kSecAttrKeyTypeRSA
+	internal let ALGORITHM: SecKeyAlgorithm = .rsaEncryptionOAEPSHA512AESGCM
 }
 
 
-class SMDecryptor {
-	private let KEY_SIZE = 2048
-	private let KEY_TYPE = kSecAttrKeyTypeRSA
+class SMDecryptor:SMCryptography {
 	private let privateKeyString: String
-		
-	private let ALGORITHM: SecKeyAlgorithm = .rsaEncryptionOAEPSHA512AESGCM
-
 	private var PRIVATE_ATTRIBUTES: [String: Any] {
 		return [
 			kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
@@ -50,7 +57,7 @@ class SMDecryptor {
 			kSecAttrKeySizeInBits as String: KEY_SIZE,
 		]
 	}
-
+	
 	init(privateKeyString: String) {
 		self.privateKeyString = privateKeyString
 	}
@@ -62,11 +69,38 @@ class SMDecryptor {
 	}
 }
 
+class SMEncryptor: SMCryptography {
+	private let publicKeyString: String
+	private var PUBLIC_ATTRIBUTES: [String: Any] {
+		return [
+			kSecAttrKeyClass as String: kSecAttrKeyClassPublic,
+			kSecAttrKeyType as String: KEY_TYPE,
+			kSecAttrKeySizeInBits as String: KEY_SIZE,
+		]
+	}
+	
+	init(publicKeyString: String) {
+		self.publicKeyString = publicKeyString
+	}
+	
+	func encrypt(_ cfData: CFData) -> CFData? {
+		guard let secKeyData = Data(base64Encoded: publicKeyString), let secKey = SecKeyCreateWithData(secKeyData as CFData, PUBLIC_ATTRIBUTES as CFDictionary, nil) else {return nil}
+		guard let cipherText = SecKeyCreateEncryptedData(secKey, ALGORITHM, cfData, nil) else {return nil}
+		return cipherText
+	}
+}
+
 extension String {
 	func decrypt(using privateKeyString: String) -> String? {
 		guard let data = Data(base64Encoded: self) as CFData? else {return nil}
 		guard let decryptedData = SMDecryptor(privateKeyString: privateKeyString).decrypt(data) else {return nil}
 		return String(decoding: decryptedData as Data, as: UTF8.self)
+	}
+	
+	func encrypt(using publicKeyString: String) -> String? {
+		guard let data = self.description.data(using: .utf8) as CFData? else {return nil}
+		guard let cipherData = SMEncryptor(publicKeyString: publicKeyString).encrypt(data) else {return nil}
+		return (cipherData as Data).base64EncodedString()
 	}
 }
 
